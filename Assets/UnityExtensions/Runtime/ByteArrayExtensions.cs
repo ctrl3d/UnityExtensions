57362d1df4ba6d794ae233acdbefe952a93eda56
vsCtrl <ctrl@vivestudios.com>
2025-08-07 11:59:15 +0900

    `ByteArrayExtensions` 유틸리티 추가: 바이트 배열을 다양한 형식(16진수, ASCII, 비트 문자열 등)으로 변환하는 메서드 제공.

---
+++ Assets/UnityExtensions/Runtime/ByteArrayExtensions.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Linq;
+using System.Text;
+
+namespace work.ctrl3d
+{
+    public static class ByteArrayExtensions
+    {
+        // 16진수 문자열
+        public static string ToHexString(this byte[] bytes) =>
+            string.Join(" ", bytes.Select(b => b.ToString("X2")));
+
+        // 16진수 문자열 (구분자 없음)
+        public static string ToHexStringCompact(this byte[] bytes) =>
+            string.Concat(bytes.Select(b => b.ToString("X2")));
+        
+        // 16진수와 ASCII를 함께 표시
+        public static string ToHexDump(this byte[] bytes, int bytesPerLine = 16)
+        {
+            var sb = new StringBuilder();
+
+            for (var i = 0; i < bytes.Length; i += bytesPerLine)
+            {
+                // 주소 표시
+                sb.Append($"{i:X8}: ");
+
+                // 16진수 표시
+                var lineBytes = bytes.Skip(i).Take(bytesPerLine).ToArray();
+                foreach (var b in lineBytes)
+                {
+                    sb.Append($"{b:X2} ");
+                }
+
+                // 빈 공간 채우기
+                for (var j = lineBytes.Length; j < bytesPerLine; j++)
+                {
+                    sb.Append("   ");
+                }
+
+                // ASCII 표시
+                sb.Append("| ");
+                foreach (var b in lineBytes)
+                {
+                    var c = b is >= 32 and <= 126 ? (char)b : '.';
+                    sb.Append(c);
+                }
+
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+
+        // 숫자 배열로 표시
+        public static string ToNumberString(this byte[] bytes) =>
+            $"[{string.Join(", ", bytes.Select(b => b.ToString()))}]";
+
+        // 비트 문자열로 표시
+        public static string ToBinaryString(this byte[] bytes) =>
+            string.Join(" ", bytes.Select(b => Convert.ToString(b, 2).PadLeft(8, '0')));
+
+
+        // 상세 정보와 함께 표시
+        public static string ToDetailedString(this byte[] bytes, Encoding encoding = null)
+        {
+            encoding ??= Encoding.UTF8;
+
+            var sb = new StringBuilder();
+            sb.AppendLine($"Length: {bytes.Length} bytes");
+            sb.AppendLine($"Hex: {bytes.ToHexString()}");
+
+            try
+            {
+                var text = encoding.GetString(bytes);
+                sb.AppendLine($"Text ({encoding.EncodingName}): \"{text}\"");
+            }
+            catch
+            {
+                sb.AppendLine("Text: [Binary data - cannot decode as text]");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
